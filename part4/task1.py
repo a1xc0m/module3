40b257aba42a0ea69c21f5169c0a382769e346ec
a1xc0m <ialexx.ak@gmail.com>
2021-11-21 23:08:08 +0300

    add task

--- part4/task1.py
+++ part4/task1.py
@@ -1,30 +1,27 @@
 import json
 import os.path
 
-
-def fn_register(log, pwd):
+def register(login, passwd):
     noreg = True
     with open('data.json', 'r') as file:
         users_data = json.load(file)
     for i in users_data.keys():
-        if i == log:
+        if i == login:
             noreg = False
     if noreg:
-        users_data[log] = pwd
+        users_data[login] = passwd
         with open('data.json', 'w') as file:
             json.dump(users_data, file) 
     return noreg
 
-
-def fn_login(log, pwd):
+def login_function(login, passwd):
     reg = False
     with open('data.json', 'r') as file:
         users_data = json.load(file)
     for x, y in users_data.items():
-        if x == log and y == pwd:
+        if x == login and y == passwd:
             reg = True      
     return reg
-
 
 data = {}
 if os.path.exists('data.json'):
@@ -37,12 +34,12 @@ select = int(input('0 = Registration, 1 = Door: '))
 login = input('Login: ')
 passwd = input('Password: ')
 if select == 0:
-    if fn_register(login, passwd):
+    if register(login, passwd):
         print('User is added')
     else:
         print('User is exist')
 else:
-    if fn_login(login, passwd):
+    if login_function(login, passwd):
         print('Successful login')
     else:
         print('Login/password incorrected')
